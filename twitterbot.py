1b9c07dc48d221e0f6e913e231a4ef640ecf1614
Monica Babaian <monica.babaian@gmail.com>
2018-02-07 14:15:10 -0600

    Cleaned up twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -1,17 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# 1 - Create variables for key, secret, token. These codes are obtained by creating a Twitter app at 
-#       https://apps.twitter.com/
-
-# 2 - This website has tons of great quotes about libraries: 
-#       https://ebookfriendly.com/best-quotes-about-libraries-librarians/
-
-# 3 - This tutorial from Mary Dickson Diaz (@marythought) was particularly helpful in creating the bot 
-#       http://marydickson.com/build-a-twitter-bot-with-python/
-
-# The textfile for this bot is library_quotes.txt
-# To run the program from the command line, type 'python twitterbot.py library_quotes.txt'
-
 import tweepy, sys, time
 
 from credentials import *
