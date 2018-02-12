2a331b95a018a6ad311d24e925b6308161e54fdb
Monica Babaian <monica.babaian@gmail.com>
2018-02-12 13:24:35 -0600

    Added feature for random tweets

--- twitterbot.py
+++ twitterbot.py
@@ -1,6 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# 1 - Create variables for key, secret, token. These codes are obtained by creating a Twitter app at 
+#       https://apps.twitter.com/
+
+# 2 - This website has tons of great quotes about libraries: 
+#       https://ebookfriendly.com/best-quotes-about-libraries-librarians/
+
+# 3 - This tutorial from Mary Dickson Diaz (@marythought) was particularly helpful in creating the bot 
+#       http://marydickson.com/build-a-twitter-bot-with-python/
+
+# The textfile for this bot is library_quotes.txt
+# To run the program from the command line, type 'python twitterbot.py library_quotes.txt'
+
 import tweepy, sys, time
 
-from credentials import *
+from whs_credentials import *
 
 argfile = str(sys.argv[1])
 
@@ -14,7 +28,14 @@ filename = open(argfile, 'r')
 f=filename.readlines()
 filename.close()
 
-for line in f: 
-    print("NOW TWEETING: " + line)
-    api.update_status(line)
-    time.sleep(300) # Tweet every 5 minutes when testing
+if __name__ == "__main__":
+	
+	while True:
+		with open(argfile) as f:
+			lines = f.readlines()
+			rand_line_num = randint(0,len(lines)-1)
+			line = lines[rand_line_num]
+            
+		print("NOW TWEETING: " + line.strip())
+		api.update_status(line.strip())
+		time.sleep(36000) # tweet every hour
